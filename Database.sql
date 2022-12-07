a9b189b4f52cfd7da26cbdcb23a2e66511f7798b
SAKAI Riho <b13913rs@gmail.com>
2022-12-07 15:00:09 +0900

    データベースにデータを追加した

--- Database.sql
+++ Database.sql
@@ -1,1 +1,31 @@
-CREATE DATABASE ToDoList
+CREATE DATABASE ToDoList;
+
+SHOW databases;
+
+USE ToDoList;
+
+CREATE TABLE Task(
+  id int auto_increment,
+  name varchar(255),
+  progress TINYINT,
+  PRIMARY KEY(id)
+);
+
+INSERT into Task
+(
+  name, progress)
+ values(
+  '今週の献立を考える',0
+  );
+INSERT into Task
+(
+  name, progress)
+ values(
+  'Java Gold取得',1
+  );
+  INSERT into Task
+(
+  name, progress)
+ values(
+  'Java Silver取得',2
+  );
